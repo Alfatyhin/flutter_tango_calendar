ff152ee18f3ec55769ec45b074cf42a51dece060
Alex-Dell <virikidorhom@gmail.com>
2023-05-02 13:53:42 +0300

    зачистка импорта тестовой страницы

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:tango_calendar/pages/fests.dart';
 import 'pages/calendars.dart';
 import 'pages/start_page.dart';
 
